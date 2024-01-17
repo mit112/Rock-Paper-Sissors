9c378fea938c0c5fed9f6896316b0a73b024c6f6
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-01-17 01:31:00 -0500

    Initial Commit

---
+++ Rock Paper Sissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rock Paper Sissors
+//
+//  Created by Mit Sheth on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rock Paper Sissors/Rock_Paper_SissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_SissorsApp.swift
+//  Rock Paper Sissors
+//
+//  Created by Mit Sheth on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_SissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
